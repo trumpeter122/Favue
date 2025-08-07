79d3883b3d337c6fc3f71319bf0faa4902feafed
trumpeter122 <186272455+trumpeter122@users.noreply.github.com>
2025-08-07 17:04:57 +0800

    ✨ Add Tasker page

--- frontend/src/components/Common/SidebarItems.tsx
+++ frontend/src/components/Common/SidebarItems.tsx
@@ -10,6 +10,7 @@ const items = [
   { icon: FiHome, title: "Dashboard", path: "/" },
   { icon: FiBriefcase, title: "Items", path: "/items" },
   { icon: FiSettings, title: "User Settings", path: "/settings" },
+  { icon: FiHome, title: "Tasker", path: "/tasker"}
 ]
 
 interface SidebarItemsProps {
